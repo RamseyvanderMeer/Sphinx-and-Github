6ae747b6f774571dc116a4a7d809dcea5732edc9
Ramsey <ramvandermeer@gmail.com>
2022-04-06 10:13:18 -0500

    Add functions

---
+++ example1.py
@@ -0,0 +1,21 @@
+def func1(arg1, arg2):
+    """
+    set arg1 equal to arg2
+
+    Args:
+        arg1 (_type_): any
+        arg2 (_type_): any
+
+    Returns:
+        _type_: any
+    """
+    arg1 = arg2
+    return arg1
+
+def func2():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return none
